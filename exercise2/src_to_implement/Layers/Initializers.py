b7fe20579cc43ad605c68d087d99166751dee1c4
SachinBM-CE <sachin.b.manjunath@fau.de>
2022-11-29 23:21:30 +0100

    edited code

--- exercise2/src_to_implement/Layers/Initializers.py
+++ exercise2/src_to_implement/Layers/Initializers.py
@@ -2,14 +2,16 @@ import numpy as np
 
 class Constant:
     def __init__(self, constant_value = 0.1):
+
         self.constant_value = constant_value
         self.kernel_height = None
         self.kernel_width = None
 
     def initialize(self, weights_shape, fan_in, fan_out):
+
         self.kernel_height, self.kernel_width = self.weights_shape
+
         self.weights = np.full((self.weights_shape), self.constant_value)
-        self.weights = np.random.rand(self.weights_shape, self.fan_in, self.fan_in)
 
         return self.weights
 
