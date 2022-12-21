212c53e219c3850bd613bf900c0ad584c8bf156e
SachinBM-CE <sachin.b.manjunath@fau.de>
2022-12-21 18:12:08 +0100

    base & NN layer

---
+++ RNN/src_to_implement/Layers/Base.py
@@ -0,0 +1,10 @@
+# Base Layer Creation
+import numpy as np
+
+
+class BaseLayer:
+
+    def __init__(self):
+        self.trainable = False
+        self.testing_phase = False
+        self.weights = np.array([])
