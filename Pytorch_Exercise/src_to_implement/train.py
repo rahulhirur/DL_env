fdd4f0886ad28f5723a4cfc84f6f8c7e3d59aa08
Sachin Bharadwaj M <119333484+SachinBM-CE@users.noreply.github.com>
2023-01-24 17:49:03 +0100

    Update train.py
    Code to create hyperparameter text file

--- Pytorch_Exercise/src_to_implement/train.py
+++ Pytorch_Exercise/src_to_implement/train.py
@@ -10,18 +10,25 @@ import os
 import datetime
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
+# Hyper-parameters for ResNet
+testSize = 0.3
+batchSize = 2
+learnRate = 0.001
+wtDecay = 0.0001
+Patience = 50
+Epochs = 1000
 
 # load the data from the csv file and perform a train-test-split
 # this can be accomplished using the already imported pandas and sklearn.model_selection modules
 
 tab = pd.read_csv('data.csv', sep=';')
-train_tab, val_tab = train_test_split(tab, test_size=0.3, random_state=42)
+train_tab, val_tab = train_test_split(tab, test_size=testSize, random_state=42)
 
 # set up data loading for the training and validation set each using t.utils.data.DataLoader and
 # ChallengeDataset objects
 
-train_dl = t.utils.data.DataLoader(ChallengeDataset(train_tab, 'train'), batch_size=16, shuffle=True)
-val_dl = t.utils.data.DataLoader(ChallengeDataset(val_tab, 'val'), batch_size=16, shuffle=True)
+train_dl = t.utils.data.DataLoader(ChallengeDataset(train_tab, 'train'), batch_size=batchSize, shuffle=True)
+val_dl = t.utils.data.DataLoader(ChallengeDataset(val_tab, 'val'), batch_size=batchSize, shuffle=True)
 
 # create an instance of our ResNet model
 model = model.ResNet()
@@ -30,13 +37,13 @@ model = model.ResNet()
 criterion = t.nn.BCELoss()
 
 # set up the optimizer (see t.optim)
-optimizer = t.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+optimizer = t.optim.Adam(model.parameters(), lr=learnRate, weight_decay=wtDecay)
 
 # create an object of type Trainer and set its early stopping criterion
-train_1 = Trainer(model, criterion, optimizer, train_dl=train_dl, val_test_dl=val_dl, cuda=True, early_stopping_patience=200)
+train_1 = Trainer(model, criterion, optimizer, train_dl=train_dl, val_test_dl=val_dl, cuda=True, early_stopping_patience=Patience)
 
 # go, go, go... call fit on trainer
-res = train_1.fit(epochs=1000)
+res = train_1.fit(epochs=Epochs)
 
 # plot the results
 plt.plot(np.arange(len(res[0])), res[0], label='train loss')
@@ -44,11 +51,22 @@ plt.plot(np.arange(len(res[1])), res[1], label='val loss')
 plt.yscale('log')
 plt.legend()
 
+dtStamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
 # save the figure with name that includes date and time
-fig_file = 'loss_' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.png'
-onnx_file = 'resnet_' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.onnx'
+fig_file = 'loss_' + dtStamp + '.png'
+onnx_file = 'resnet_' + dtStamp + '.onnx'
 
 plt.savefig(fig_file)
 
 # save the model as onnx file
 train_1.save_onnx(onnx_file)
+
+# Model Text File
+txt_file = dtStamp + '.txt'
+lines = [testSize, batchSize, learnRate, wtDecay, Patience, Epochs]
+with open(txt_file, 'w') as f:
+    for line in lines:
+        f.write(line)
+        f.write('\n')
+f.close
