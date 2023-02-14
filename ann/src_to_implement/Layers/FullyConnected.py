4fa424bee68359d0c92798800cc200deae28f550
Sachin Bharadwaj M <119333484+SachinBM-CE@users.noreply.github.com>
2023-02-14 17:23:12 +0100

    comments

--- ann/src_to_implement/Layers/FullyConnected.py
+++ ann/src_to_implement/Layers/FullyConnected.py
@@ -9,25 +9,27 @@ class FullyConnected(BaseLayer):
     def __init__(self, input_size, output_size):
 
         super().__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.trainable = True
+        self.forward_output = None
         self.backward_output = None
         self.input_tensor = None
         self._optimizer = None
         self._gradient_weights = None
-        self.forward_output = None
-        self.input_size = input_size
-        self.output_size = output_size
-        self.trainable = True
         # Initialize weights to carry bias in last row
         self.weights = np.random.rand(self.input_size + 1, self.output_size)
 
     def forward(self, input_tensor):
+        
         # wx+b*1
-
+        # same number of rows as input_tensor and an additional column of ones
         self.input_tensor = np.concatenate((input_tensor, np.ones((input_tensor.shape[0], 1))), axis=1)
 
         self.forward_output = np.dot(self.input_tensor, self.weights)
         return self.forward_output
 
+    
     @property
     def optimizer(self):
 
@@ -48,10 +50,12 @@ class FullyConnected(BaseLayer):
 
         self._gradient_weights = value
 
+        
     def backward(self, error_tensor):
 
         self.backward_output = np.dot(error_tensor, self.weights.transpose())
         self.gradient_weights = np.dot(self.input_tensor.transpose(), error_tensor)
+        
         if self.optimizer is not None:
             self.weights = self.optimizer.calculate_update(self.weights, self.gradient_weights)
 
