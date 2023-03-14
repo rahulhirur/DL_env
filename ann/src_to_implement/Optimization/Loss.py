3cbfdbeb393204b8982beb46fc9bed015bbe3b1a
Sachin Bharadwaj M <119333484+SachinBM-CE@users.noreply.github.com>
2023-03-15 00:12:32 +0100

    Update Loss.py

--- ann/src_to_implement/Optimization/Loss.py
+++ ann/src_to_implement/Optimization/Loss.py
@@ -1,27 +1,42 @@
 import numpy as np
-
 from Layers.Base import BaseLayer
 
 
 class CrossEntropyLoss(BaseLayer):
 
     def __init__(self):
+        
         super().__init__()
-        self.label_tensor = None
-        self.backward_output = None
-        self.forward_output = None
+        
         self.input_tensor = None
+        self.label_tensor = None
+        
+        self.forward_output = None
+        self.backward_output = None
+        
 
     def forward(self, prediction_tensor, label_tensor):
-        # Compensate zero log output
+        
+        """
+        Computes the loss value as per CrossEntropy Loss formula accumulated over the batch
+        """
 
         self.label_tensor = label_tensor
-        self.input_tensor = np.finfo(float).eps + prediction_tensor
+        
+        # Epsilon is added to avoid log(0)=undefined
+        self.input_tensor = np.finfo(float).eps + prediction_tensor 
         self.forward_output = np.sum(-np.log(np.sum(self.input_tensor * label_tensor, axis=1)))
 
         return self.forward_output
+    
 
     def backward(self, label_tensor):
+        
+        """
+        - The backpropagation starts here, hence no error tensor is needed
+        - Instead, we need the label tensor
+        """
 
         self.backward_output = - (label_tensor / self.input_tensor)
+        
         return self.backward_output
