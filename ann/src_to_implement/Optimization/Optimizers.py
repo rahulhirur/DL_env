1452f871504b600261628c734b276388d6c57a7d
Sachin Bharadwaj M <119333484+SachinBM-CE@users.noreply.github.com>
2023-03-15 00:18:26 +0100

    Update Optimizers.py

--- ann/src_to_implement/Optimization/Optimizers.py
+++ ann/src_to_implement/Optimization/Optimizers.py
@@ -1,13 +1,10 @@
-# Optimizer File
-
 import numpy as np
 
 
-# Creating the SGD Class
-
 class Sgd:
-    # __init__ is the constructor class
+    
     def __init__(self, learning_rate):
+        
         try:
             if type(learning_rate) == int or type(learning_rate) == float:
                 learning_rate = float(learning_rate)
@@ -20,7 +17,12 @@ class Sgd:
             print('\n//////')
             print(msg)
             print('//////')
+            
 
     def calculate_update(self, weight_tensor, gradient_tensor):
-
+        
+        """"
+        Returns the updated weights according to the basic gradient descent update scheme.
+        """"
+        
         return np.subtract(weight_tensor, (self.learning_rate * gradient_tensor))
