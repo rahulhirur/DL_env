670c8698a9798056e287f5aeeecffcd9641f0abf
Sachin Bharadwaj M <119333484+SachinBM-CE@users.noreply.github.com>
2023-03-15 00:00:12 +0100

    Update SoftMax.py

--- ann/src_to_implement/Layers/SoftMax.py
+++ ann/src_to_implement/Layers/SoftMax.py
@@ -1,23 +1,32 @@
 import numpy as np
-
 from Layers.Base import BaseLayer
 
 
 class SoftMax(BaseLayer):
 
     def __init__(self):
+        
         super().__init__()
+        
         self.forward_output = None
         self.backward_output = None
+        
 
     def forward(self, input_tensor):
+        
+        """"
+        Returns the estimated class probabilities for each row representing an element of the batch 
+        """"
+        
         p_exp = np.exp(input_tensor - np.max(input_tensor, axis=1, keepdims=True))
         self.forward_output = p_exp / p_exp.sum(axis=1, keepdims=True)
 
         return self.forward_output
+    
 
     def backward(self, error_tensor):
 
-        self.backward_output = self.forward_output * (error_tensor - np.sum(error_tensor * self.forward_output, axis=1,
+        self.backward_output = self.forward_output * (error_tensor - np.sum(error_tensor * self.forward_output, 
+                                                                            axis=1,
                                                                             keepdims=True))
         return self.backward_output
