0f5cff2d857b11f398abc5311b28ab34f1951434
Sachin Bharadwaj M <119333484+SachinBM-CE@users.noreply.github.com>
2023-03-24 23:45:39 +0100

    Update data.py

--- Pytorch_Exercise/src_to_implement/data.py
+++ Pytorch_Exercise/src_to_implement/data.py
@@ -7,32 +7,40 @@ import numpy as np
 import torchvision as tv
 import pandas as pd
 
+
 class AddGaussianNoise:
+    
     def __init__(self, mean=0., std=1.):
+        
         self.std = std
         self.mean = mean
+        
 
     def __call__(self, tensor):
+        
         return tensor + t.randn(tensor.size()) * self.std + self.mean
+    
 
     def __repr__(self):
+        
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+    
 
 class ChallengeDataset(Dataset):
-    # TODO implement the Dataset class according to the description
 
     def __init__(self, data, mode: str):
-        self.data = data
         
+        self.data = data
         self.mode = mode
+        
         train_mean = [0.59685254, 0.59685254, 0.59685254]
         train_std = [0.16043035, 0.16043035, 0.16043035]
+        
         if mode == 'train':
             self._transform = tv.transforms.Compose([
                 tv.transforms.ToPILImage(),
                 tv.transforms.RandomApply([tv.transforms.ColorJitter(brightness= (0.6,2), contrast=0.6)], p=0.45),
                 tv.transforms.ToTensor(),
-
                 tv.transforms.RandomHorizontalFlip(p=0.5),
                 tv.transforms.RandomVerticalFlip(p=0.5),
                 #tv.transforms.RandomErasing(p=0.1, ratio=(0.54, 0.4), scale=(0.02, 0.04), value=0),
@@ -49,11 +57,16 @@ class ChallengeDataset(Dataset):
                 tv.transforms.Normalize(mean=train_mean, std=train_std, inplace=False)])
         else:
             print('Invalid mode given')
+            
 
     def __len__(self):
+        
         return len(self.data)
+    
 
     def __getitem__(self, index):
+        
         img = self._transform(gray2rgb(imread(self.data.iloc[index, 0])))
         labels = t.tensor((self.data.iloc[index, 1], self.data.iloc[index, 2]))
+        
         return img, labels
