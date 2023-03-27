39e902ef71e0da1a733cb6261fb09950a0fa9b7d
Sachin Bharadwaj M <119333484+SachinBM-CE@users.noreply.github.com>
2023-03-27 12:27:39 +0200

    Update Flatten.py

--- RNN/src_to_implement/Layers/Flatten.py
+++ RNN/src_to_implement/Layers/Flatten.py
@@ -1,23 +1,24 @@
 import numpy as np
-
 from Layers.Base import BaseLayer
 
 
 class Flatten(BaseLayer):
+    
     def __init__(self):
-
+        
         super().__init__()
-
         self.fwd_shape = None
         self.forward_output = None
         self.backward_output = None
 
+        
     def forward(self, input_tensor):
 
         self.fwd_shape = input_tensor.shape
         self.forward_output = input_tensor.reshape(input_tensor.shape[0], int(input_tensor.size/input_tensor.shape[0]))
         return self.forward_output
 
+    
     def backward(self, error_tensor):
 
         self.backward_output = error_tensor.reshape(self.fwd_shape)
